d472caba8c271e5bd426f0e9f82a063ef4d4da20
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-27 16:24:18 -0400

    Override GetHashCode in Person class.

--- ObjectOverrides/Person.cs
+++ ObjectOverrides/Person.cs
@@ -8,10 +8,13 @@ namespace ObjectOverrides
 {
     class Person
     {
+        //Properties.
         public string FirstName { get; set; } = "";
         public string LastName { get; set; } = "";
         public int Age { get; set; }
+        public string SSN { get; set; } = "";
 
+        //Constructors.
         public Person(string fName, string lName, int personAge)
         {
             FirstName = fName;
@@ -19,7 +22,10 @@ namespace ObjectOverrides
             Age = personAge;
         }
         public Person() { }
+
+        // Method Overrides.
         public override string ToString() => $"[First Name: {FirstName}; Last Name: {LastName}; Age: {Age}]";
         public override bool Equals(object obj) => obj?.ToString() == ToString();
+        public override int GetHashCode() => SSN.GetHashCode();
     }
 }
